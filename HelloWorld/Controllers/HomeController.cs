81d6142e3c85fe470a3782cb2417003a9ad4c391
alphanut <arnaud.seguin@gmail.com>
2019-10-09 00:31:57 +0200

    Hello World

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index(string id)
+        {
+            return @"<html>
+                    <head>
+                        <title>Hello World MVC !</title>
+                    </head>
+                    <body>
+                        <p>Hello <span style=""color:red"">" + id + @"</span></p>
+                    </body>
+                    </html>";
+        }
+
+        public ActionResult Index2(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return View("Error");
+
+            ViewData["Name"] = id;
+            ViewBag.Nom = "Seguin"; // on ne peut pas utiliser Name car déjà utilisé par ViewData
+            return View();
+        }
+
+        public ActionResult ListeClients()
+        {
+            var clients = new Clients();
+            ViewBag.Clients = clients.GetListClients();
+            return View();
+        }
+
+        public ActionResult ChercheClient(string id)
+        {
+            ViewBag.Nom = id;
+            var clients = new Clients();
+            var client = clients.GetListClients().FirstOrDefault(c => c.Nom == id);
+            if (client != null)
+            {
+                ViewBag.Age = client.Age;
+                return View("Trouve");
+            }
+
+            return View("NonTrouve");
+        }
+    }
+}
---
+++ HelloWorld/Models/Clients.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class Clients
+    {
+        public List<Client> GetListClients()
+        {
+            return new List<Client>
+            {
+                new Client {Age = 33, Nom = "Nicola"},
+                new Client {Age = 15, Nom = "Maxime"},
+                new Client {Age = 65, Nom = "Albert"}
+            };
+        }
+    }
+}
