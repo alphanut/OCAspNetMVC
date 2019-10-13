d2b0d3b2411bdef22758ce5ff6408e382ffdd835
alphanut <arnaud.seguin@gmail.com>
2019-10-14 00:08:49 +0200

    model

--- HelloWorld/App_Start/RouteConfig.cs
+++ HelloWorld/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace HelloWorld
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Meteo",
+                url: "{jour}/{mois}/{annee}",
+                defaults: new { controller = "Meteo", action = "Afficher" },
+                constraints: new { jour = @"\d+", mois = @"\d+", annee = @"\d+" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
